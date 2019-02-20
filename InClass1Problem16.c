2bbce33e6adc4d0df24f66f3986797466a05965d
ajhumphreys <47014883+ajhumphreys@users.noreply.github.com>
2019-02-19 20:01:22 -0800

    Add files via upload

---
+++ InClass1Problem16.c
@@ -0,0 +1,15 @@
+//reading two items a string and int
+#include <stdio.h>
+int main( ) {
+
+   char str[100];
+   int i;
+
+   printf( "Enter a value: ");
+   fflush(stdout); //ECLIPSE BUG
+   scanf("%s %d",str, &i);
+
+   printf( "\nYou entered: %s %d ",str, i);
+
+   return 0;
+}
